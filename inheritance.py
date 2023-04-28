b0354762911ddb68e5a5d17a47e02f921845b686
Eugen Venieri <106976929+Venieri-E@users.noreply.github.com>
2023-04-28 16:39:04 +0300

    New commit

--- inheritance.py
+++ inheritance.py
@@ -34,26 +34,32 @@ class new_user(Person):
     def __init__(self, name, age, height, weight):
         """"initialisation of parent class attributes"""
         super().__init__(name, age, height, weight)
+        self.job = " Programmer"
+
+    def get_job(self):
+        """"add job position"""
+        print("Position is :" + str(self.job))
 
 class new_user1(Person):
     """"Create class 'Job '"""
     def __init__(self, name, age, height, weight):
         """"initialisation of parent class attributes"""
         super().__init__(name, age, height, weight)
+        self.job1 = " QA Engineer"
+
+    def get_job1(self):
+        """"add job position"""
+        print("Position is :" + str(self.job1))
 
 new_user = new_user("Danila", 22, 159, 64)
 new_user.update_weight(68)
 new_user.description_person()
-
-new_user1 = new_user1("Anton", 22, 160, 60)
+new_user.get_job()
+new_user1 = new_user1("Jenea" , 28 , 180 , 85)
 new_user1.description_person()
-
-man = Person ("Jenea" , 28 , 180 , 85)
-women = Person ("Dasha" , 26 , 160 , 55)
-women1 = Person ("Dana" , 21 , 158 , 52)
-
-man.description_person()
-women.description_person()
-women1.description_person()
-
+new_user1.get_job1()
+#
+# man = Person("Anton", 22, 160, 60)
+# women = Person ("Dasha" , 26 , 160 , 55)
+# women1 = Person ("Dana" , 21 , 158 , 52)
 
