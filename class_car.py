7d6d1812414e2fc391f0a5b04e3a4a559e6694de
Eugen Venieri <106976929+Venieri-E@users.noreply.github.com>
2023-04-28 20:34:04 +0300

    re

--- class_car.py
+++ class_car.py
@@ -21,12 +21,12 @@ class new_truck_1(Cars):
     def __init__(self, model, year, capacity, price, milleage, wheels):
          """"initialisation of parent class attributes"""
          super().__init__(model, year, capacity, price, milleage, wheels)
-         self.bus = "New model "
+         self.new_car_2 = "since 2005"
 
 
     def get_new_truck1(self):
         """"add truck position"""
-        print("New Truck :  " + str(self.bus))
+        print("Old model " + str(self.new_car_2))
 
 
 class new_truck2(Cars):
@@ -34,10 +34,11 @@ class new_truck2(Cars):
     def __init__(self, model, year, capacity, price, milleage, wheels):
         """"initialisation of parent class attributes"""
         super().__init__(model, year, capacity, price, milleage, wheels)
+        self.new_car_2 = "Bought this year"
 
     def get_new_truck2(self):
         """"add truck position"""
-        print("New Truck :  " + str(self.new_car_2))
+        print("New car in park, " + str(self.new_car_2))
 
 
 car_1 =Cars("BMW", 1995, 2, 5000, 313456, 4)
@@ -45,7 +46,9 @@ car_1.description_car()
 
 new_truck_1 = new_truck_1("Mann", 2002, 12, 76883, 98343, 8)
 new_truck_1.description_car()
+new_truck_1.get_new_truck1()
 
 new_truck2 = new_truck2("DAF", 2005, 16, 1000000, 182343, 8)
 new_truck2.description_car()
+new_truck2.get_new_truck2()
 
